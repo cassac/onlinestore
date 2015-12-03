4818dcae9a5e91c60925b9a557121ae7482b343f
cassac <hicassac@gmail.com>
2015-12-02 19:26:20 -0800

    adds search functionality

--- onlinestore/products/urls.py
+++ onlinestore/products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.all_products, name='all_products'),
+    url(r'^search/', views.search_products, name='search_products'),
     url(r'^product/(?P<slug>[\w-]+)/$', views.single_product, name='single_product'),
 ]
--- onlinestore/products/views.py
+++ onlinestore/products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.db.models import Q
+
 from .models import Product
 
 def all_products(request):
@@ -10,3 +12,13 @@ def single_product(request, slug):
 	product = Product.objects.get(slug=slug)
 	context = {"product": product}
 	return render(request, 'products/single.html', context)
+
+def search_products(request):
+	search_term = request.GET.get('term')
+	search_results = Product.objects.filter(
+					Q(title__icontains=search_term) |
+					Q(description__icontains=search_term)
+				)
+	context = {"products": search_results,
+			   "search_term": search_term}
+	return render(request, 'products/all.html', context)
