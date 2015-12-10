df08122d5c2cc062c7d8ae70e1600e2206a369f7
cassac <hicassac@gmail.com>
2015-12-09 16:14:44 -0800

    adds checkout view and template

--- onlinestore/orders/models.py
+++ onlinestore/orders/models.py
@@ -2,12 +2,24 @@ from django.db import models
 from django.conf import settings
 
 from carts.models import Cart
+from accounts.models import UserMailingAddress, UserBillingAddress
         
+STATUS_CHOICES = (
+		("Processing", "Processing"),
+		("Cancelled", "Cancelled"),
+		("Shipped", "Shipped"),
+		("Returned", "Returned"),
+		("Completed", "Completed")		
+	)
 
 class Order(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL)
 	cart = models.ForeignKey(Cart)
+	mailing_address = models.ForeignKey(UserMailingAddress)
+	billing_address = models.ForeignKey(UserBillingAddress)	
+	status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="Processing")
 	subtotal = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+	tax = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 	total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True) 	
--- onlinestore/orders/views.py
+++ onlinestore/orders/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
+from carts.models import Cart
+from .models import Order
+
+@login_required
 def my_orders(request):
 	return HttpResponse("Your orders are displayed here")
 
+@login_required
 def new_order(request):
-	return HttpResponse("This is your new order page")
+	user = request.user
+	billing_address = user.userbillingaddress_set.last()
+	mailing_address = user.usermailingaddress_set.last()
+	cart_id = request.session.get('cart_id')
+	cart = Cart.objects.get(id=cart_id)
+	order_total = cart.get_total()
+	context = {'billing_address': billing_address, 
+			   'mailing_address': mailing_address,
+			   'order_total': order_total
+			   }
+
+	if request.method == 'POST':
+		card_num = request.POST['card_num']
+		card_cvv = request.POST['card_cvv']
+		card_exp = request.POST['card_exp']
+		# Validate and do stuff with credit card
+
+		order = Order(
+			user=user,
+			cart=cart,
+			mailing_address=mailing_address,
+			billing_address=billing_address,
+			subtotal=cart.get_subtotal(),
+			tax=cart.get_tax(),
+			total=cart.get_total()
+			)
+		order.save()
+
+		del request.session['cart_id']
+
+		messages.add_message(request, messages.SUCCESS, 'Order submitted uccessfully.')
+		return HttpResponseRedirect(reverse('my_orders'))
+	return render(request, 'orders/new.html', context)
