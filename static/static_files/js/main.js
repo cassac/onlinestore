addb020aca637c8dab29da68829b7bb655a6889b
cassac <hicassac@gmail.com>
2016-03-09 22:21:41 -0800

    processes checkout and receives redirect if views detect blank mailing address

---
+++ static/static_files/js/main.js
@@ -0,0 +1,48 @@
+var parseShippingRates = function(ratesArray) {
+	console.log(ratesArray);
+	var listHTML = '';
+	$.each(ratesArray, function(index, value) {
+		console.log(index, value);
+		item = "<option value="+value.rate_id+">"+ index + " "+value.rate +" "+value.currency + "</option>";
+		listHTML += item;
+	});
+	console.log(listHTML);
+	$('#shippingRatesMenu').append(listHTML);
+}
+
+var getShippingRates = function(){
+
+	// setToken();
+	$.ajax({
+		url:'/shipping/rates/',
+		type: 'GET',
+		contentType: 'application/json;charset=UTF-8',
+  		dataType: "json",
+  		statusCode: {
+  			302: function(xhr) {
+  				var json = JSON.parse(xhr.responseText);
+  				var url = json.redirect_to;
+  				window.location = url;
+  			}
+  		},
+  		success: function(data){
+  			console.log(data)
+  			parseShippingRates(data);
+  		},
+  		error: function(error){
+  			console.log(error);
+  		} 
+	}) // end ajax
+
+}
+
+$(document).ready(function(){
+	$('#updateBtn').hide();
+	$('input[type="number"]').on('change', function(){
+		$('#updateBtn').show();
+	});
+
+
+	getShippingRates();
+
+});
