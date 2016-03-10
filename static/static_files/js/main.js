26b0b45efb5352752c7229dfeba0d3c65d3b5d32
cassac <hicassac@gmail.com>
2016-03-10 12:19:51 -0800

    adds csrftoken implementation and sets up ajax put request

--- static/static_files/js/main.js
+++ static/static_files/js/main.js
@@ -66,18 +66,50 @@ $(document).ready(function(){
 
 });
 
+// using jQuery
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
 $('#submitOrder').on('click', function(e){
 	e.preventDefault();
 	var totalPrice = $('#totalPrice').text();
-	var selectedShipping = $('#shippingRatesMenu').find(":selected").val();
+	var selectedShippingPrice = $('#shippingRatesMenu').find(":selected").attr('data-price');
+	var selectedShippingID = $('#shippingRatesMenu').find(":selected").val();
 	console.log('order submitted');
-	console.log(totalPrice, selectedShipping);
+	console.log(totalPrice, selectedShippingPrice, selectedShippingID);
 
 	$.ajax({
-		url:'/orders/new',
-		type: 'POST',
+		url:'/mycart/',
+		type: 'PUT',
 		contentType: 'application/json;charset=UTF-8',
   		dataType: "json",
+  		data: JSON.stringify({'rate': selectedShippingPrice, 'rate_id': selectedShippingID}),
   		success: function(r) {
   			console.log(r);
   		},
